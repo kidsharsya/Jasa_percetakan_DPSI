7fc187ae4e12c40183601665f2cbeff155a7c330
Krisna Harsya Saputra <129250588+kidsharsya@users.noreply.github.com>
2024-07-12 23:11:30 +0700

    GASS DEPLOY3

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var app = express();
 const { sequelize } = require('./models');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
